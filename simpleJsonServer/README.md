ed219afb3828db170171f43ca5efd21160145a51
ryanc1993 <ryan149347@gmail.com>
2017-10-13 23:50:07 +0800

    simple json server for frontend

---
+++ simpleJsonServer/README.md
@@ -0,0 +1,9 @@
+# Simple Json Server
+> For frontend API
+```
+# install dependencies
+yarn install
+
+# serve at localhost:5000
+yarn start
+```
---
+++ simpleJsonServer/route.js
@@ -0,0 +1,8 @@
+const candidates  = require('./db/db.json');
+
+
+module.exports = function() {
+  return {
+    candidates  : candidates,
+  }
+}
