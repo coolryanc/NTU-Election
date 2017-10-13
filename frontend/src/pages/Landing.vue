bc898139b2ea5026365a896fe897938f4f3d0650
ryanc1993 <ryan149347@gmail.com>
2017-10-13 23:58:49 +0800

    first commit for frontend

---
+++ frontend/src/pages/Landing.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .hello
+    .hu {{msg}}
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+.hu
+  color: redaa
+</style>
