46f4ec10754cffe2e4492501b51ea53d96bdcfab
ryanc1993 <ryan149347@gmail.com>
2017-11-05 07:35:17 +0800

    + json data

--- simpleJsonServer/server.js
+++ simpleJsonServer/server.js
@@ -7,5 +7,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 server.listen(5000, function () {
-  console.log('JSON Server is running');
+  console.log('JSON Server is running: localhost:5000');
+  console.log('API: http://localhost:5000/candidates')
 });
