ffc9fbf46e75ac51641e584f3f64e5a674818752
ryanc1993 <ryan149347@gmail.com>
2017-11-14 15:03:22 +0800

    + navBar

--- frontend/src/templates/TemplateTN.vue
+++ frontend/src/templates/TemplateTN.vue
@@ -2,14 +2,23 @@
   #main
     nav.navbar(role="navigation" aria-label="main navigation")
       .navbar-brand
-        router-link.navbar-item(to="/") 台大校長選舉
+        router-link(to="/")
+          img(src="/static/canIAsk.png" style="height: 84px;")
         .navbar-burger.burger
           span(v-for="i in 3")
       .navbar-menu
         .navbar-start
-          router-link.navbar-item(to="/candidate") 候選人
-          router-link.navbar-item(to="/interview") 訪問
-          a.navbar-item 投票去
+          router-link.navbar-item(to="/candidate")
+            img(src=src="https://ntusa.iconcern.tw/img/profile.png" style="height: 18px;")
+            p.subtitle.is-6 &nbsp; 候選人簡介
+          router-link.navbar-item(to="/interview")
+            img(src="https://ntusa.iconcern.tw/img/issue.png" style="height: 18px;")
+            p.subtitle.is-6 &nbsp; 校務議題立場
+          a.navbar-item
+            img(src="https://ntusa.iconcern.tw/img/vote.png" style="height: 18px;")
+            p.subtitle.is-6 &nbsp; 我要投票
+          a.navbar-item(href="https://ntusa.iconcern.tw/about.php")
+            img(src="https://ntusa.iconcern.tw/img/iConcenr_info.png" style="height: 60px;")
     section.section
       .container
         slot
@@ -38,14 +47,41 @@ export default {
 // @import "../../node_modules/bulma/sass/utilities/_all"
 // @import "../../node_modules/bulma/sass/components/navbar.sass"
 // @import "../sass/common"
-
 .navbar
   box-shadow: 0 2px 5px 0px rgba(black, 0.1)
+  padding: 12px 0
+  position: fixed
+  width: 100% !important
+  z-index: 990
+  height: 7em
+
 a
   text-decoration: none
 
+.section
+  background-color: rgba(black, 0.01)
+  padding-top: 10em
+
+.navbar-brand, .navbar-menu
+  margin-left: 2em
+
+.navbar-brand img
+  position: relative
+  // top: 20px
+
 .container
   min-height: 100vh
+  padding: 0 25px
+
+p.subtitle.is-6
+  font-size: .9em
+  font-weight: 500
+  color: rgba(black, 0.5)
+  &:hover
+    color: rgba(black, 0.6)
+.navbar-burger.burger
+  &:hover
+    background-color: transparent
 
 #main
   height: 100%
